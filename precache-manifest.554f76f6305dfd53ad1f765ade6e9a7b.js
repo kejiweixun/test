d6d7b08b1b9de0085e753583dcf0139f74c0044f
kejiweixun <kejiweixun@gmail.com>
2019-08-15 20:34:16 +0800

    Updates

---
+++ precache-manifest.554f76f6305dfd53ad1f765ade6e9a7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380a28741115e16ab21a1e3e7fff85bf",
+    "url": "/1st-learning-project/index.html"
+  },
+  {
+    "revision": "673f9318bc2bd593f36c",
+    "url": "/1st-learning-project/static/css/main.f55cc74d.chunk.css"
+  },
+  {
+    "revision": "de2c01b211b3d4a73f43",
+    "url": "/1st-learning-project/static/js/2.fd6cc176.chunk.js"
+  },
+  {
+    "revision": "673f9318bc2bd593f36c",
+    "url": "/1st-learning-project/static/js/main.1a02e1db.chunk.js"
+  },
+  {
+    "revision": "d233029d25b814b76360",
+    "url": "/1st-learning-project/static/js/runtime~main.8792cd76.js"
+  },
+  {
+    "revision": "d99f0a51b9548fb8217b5d86661bc12a",
+    "url": "/1st-learning-project/static/media/banner.d99f0a51.png"
+  },
+  {
+    "revision": "8e1c6c380a214683ac1b06287fb266de",
+    "url": "/1st-learning-project/static/media/figma.8e1c6c38.svg"
+  },
+  {
+    "revision": "35135295f76433ca601e6b7450fa90e9",
+    "url": "/1st-learning-project/static/media/plugin.35135295.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.1fa3f14d683235dcd24bc7c45437e23f.js"
+  "/1st-learning-project/precache-manifest.554f76f6305dfd53ad1f765ade6e9a7b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/1st-learning-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
