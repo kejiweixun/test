6f474b9bef4f57d53379c4f57a2e47cf855aefcd
kejiweixun <kejiweixun@gmail.com>
2019-08-15 14:00:22 +0800

    Updates

---
+++ precache-manifest.49cd7b5eac648076cf9194908aa91f1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf1fcc14880c282aa4f7b9fdc97ddcc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e8bf939c8772485ae639",
+    "url": "/my-app/static/css/main.f55cc74d.chunk.css"
+  },
+  {
+    "revision": "7755094e1ff7fc15a2d7",
+    "url": "/my-app/static/js/2.a5d5b8eb.chunk.js"
+  },
+  {
+    "revision": "e8bf939c8772485ae639",
+    "url": "/my-app/static/js/main.ca7e4d90.chunk.js"
+  },
+  {
+    "revision": "55a31d2e237ec74d037f",
+    "url": "/my-app/static/js/runtime~main.21a9b63e.js"
+  },
+  {
+    "revision": "88724b305e0e4a9531a68ddcc998c8cf",
+    "url": "/my-app/static/media/banner.88724b30.png"
+  },
+  {
+    "revision": "8e1c6c380a214683ac1b06287fb266de",
+    "url": "/my-app/static/media/figma.8e1c6c38.svg"
+  },
+  {
+    "revision": "35135295f76433ca601e6b7450fa90e9",
+    "url": "/my-app/static/media/plugin.35135295.png"
+  }
+]);
